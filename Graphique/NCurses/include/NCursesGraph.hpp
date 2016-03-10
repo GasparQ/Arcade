f75a6bef5afa111ef933eb1e38a210977fc51ad1
Victor <victor.gouet@epitech.eu>
2016-03-10 15:36:00 +0100

    lib ncurses lol

---
+++ Graphique/NCurses/include/NCursesGraph.hpp
@@ -0,0 +1,37 @@
+//
+// NCursesGraph.hpp for lib in /home/gouet_v/Rendu/semester4/CPP/cpp_arcade/Graphique/NCurses
+// 
+// Made by Victor Gouet
+// Login   <gouet_v@epitech.net>
+// 
+// Started on  Thu Mar 10 14:58:54 2016 Victor Gouet
+// Last update Thu Mar 10 15:30:33 2016 Victor Gouet
+//
+
+#ifndef NCURSESGRAP_HPP_
+# define NCURSESGRAP_HPP_
+
+# include "../../include/IGraph.hpp"
+# include "Window.hpp"
+# include "NCurses.hpp"
+# include "../../../Component/include/GameComponent.hpp"
+
+class	NCursesGraph	: public IGraph
+{
+public:
+  NCursesGraph();
+  virtual ~NCursesGraph();
+
+public:
+  virtual int eventManagment();
+  virtual void display(std::stack<AComponent *>);
+
+private:
+  void	        _gameComponent(GameComponent *) const;
+
+private:
+  ncr::Window	*gameWin;
+  ncr::Window	*_board;
+};
+
+#endif
---
+++ Graphique/NCurses/src/NCursesGraph.cpp
@@ -0,0 +1,57 @@
+//
+// NCursesGraph.cpp for lib in /home/gouet_v/Rendu/semester4/CPP/cpp_arcade/Graphique/NCurses
+// 
+// Made by Victor Gouet
+// Login   <gouet_v@epitech.net>
+// 
+// Started on  Thu Mar 10 15:05:21 2016 Victor Gouet
+// Last update Thu Mar 10 15:34:27 2016 Victor Gouet
+//
+
+#include "../include/NCursesGraph.hpp"
+
+NCursesGraph::NCursesGraph()
+{
+  NCurses::init();
+  NCurses::noEchoMode();
+  NCurses::hide_cursor();
+  _board = new ncr::Window(31, 51, 0, 0);
+  gameWin = new ncr::Window(30, 50, 1, 1, _board->getWin());
+  _board->attrON(A_REVERSE);
+  _board->makeBorder(' ', ' ', ' ');
+  _board->attrOFF(A_REVERSE);
+}
+
+NCursesGraph::~NCursesGraph()
+{
+  if (gameWin)
+    delete gameWin;
+  NCurses::destroy();
+}
+
+int	NCursesGraph::eventManagment()
+{
+  return (NCurses::getCarac());
+}
+
+void	        NCursesGraph::_gameComponent(GameComponent *gameComponent) const
+{
+  Vector2	pos = gameComponent->getPos();
+
+  gameWin->print(pos.x, pos.y, "%s", gameComponent->getSpriteText().c_str());
+}
+
+void	NCursesGraph::display(std::stack<AComponent *>	obj)
+{
+  GameComponent	*gameComponent;
+
+  gameWin->clear();
+  while (!obj.empty())
+    {
+      if ((gameComponent = dynamic_cast<GameComponent *>(obj.top())) != NULL)
+	{
+	  _gameComponent(gameComponent);
+	}
+      obj.pop();
+    }
+}
