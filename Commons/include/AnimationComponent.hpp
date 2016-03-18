bd00815f0c71e9ec71817bc25dcfaac58dbd6d99
Victor <victor.gouet@epitech.eu>
2016-03-18 14:07:32 +0100

    animationComponent

---
+++ Commons/include/AnimationComponent.hpp
@@ -0,0 +1,35 @@
+//
+// AnimationComponent.hpp for AnimationMenu in /home/gouet_v/Rendu/semester4/CPP/cpp_arcade/Commons
+// 
+// Made by Victor Gouet
+// Login   <gouet_v@epitech.net>
+// 
+// Started on  Fri Mar 18 12:04:59 2016 Victor Gouet
+// Last update Fri Mar 18 14:05:22 2016 Victor Gouet
+//
+
+#ifndef ANIMATIONCOMPONENT_HPP_
+# define ANIMATIONCOMPONENT_HPP_
+
+# include <iostream>
+# include "AComponent.hpp"
+
+class   AnimationComponent : public AComponent
+{
+public:
+  AnimationComponent(int x, int y, enum ComponentColor color,
+		     std::string const &file);
+  virtual ~AnimationComponent();
+
+private:
+  const std::string file;
+
+  /*
+  **	GETTER
+  */
+public:
+  std::string const &getFileName() const;
+};
+
+
+#endif
---
+++ Commons/src/AnimationComponent.cpp
@@ -0,0 +1,27 @@
+//
+// AnimationComponent.cpp for AnimationComponent in /home/gouet_v/Rendu/semester4/CPP/cpp_arcade/Commons
+// 
+// Made by Victor Gouet
+// Login   <gouet_v@epitech.net>
+// 
+// Started on  Fri Mar 18 12:07:28 2016 Victor Gouet
+// Last update Fri Mar 18 14:05:48 2016 Victor Gouet
+//
+
+#include "../include/AnimationComponent.hpp"
+
+AnimationComponent::AnimationComponent(int x, int y, enum ComponentColor color,
+				       std::string const &file)
+  : AComponent(x, y, color), file(file)
+{
+}
+
+AnimationComponent::~AnimationComponent()
+{
+  
+}
+
+std::string const &AnimationComponent::getFileName() const
+{
+  return (file);
+}
